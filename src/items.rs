a0230ae02928d1ab04064bb0cab8d9df5d7b9e37
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-02-22 23:39:58 -0500

    feat: add all_item_details

--- src/items.rs
+++ src/items.rs
@@ -79,6 +79,115 @@ enum Code {
     String(String),
 }
 
+// todo: make this not panic on failure
+impl Code {
+    /// Will panic if it cannot convert to i64.
+    pub fn to_i64(&self) -> i64 {
+        match self {
+            Self::Integer(x) => *x,
+            Self::String(x) => x.parse().unwrap(),
+        }
+    }
+}
+
+impl std::fmt::Display for Code {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Integer(x) => write!(f, "{}", x),
+            Self::String(x) => write!(f, "{}", x),
+        }
+    }
+}
+
+impl ItemDetails {
+    fn from_raw(item_id: u64, codes: Vec<Code>) -> Result<Self, ItemsError> {
+        let item_name = codes[0].to_string();
+
+        let acronym = {
+            if codes[1].to_string().is_empty() {
+                None
+            } else {
+                Some(codes[1].to_string())
+            }
+        };
+
+        let rap = codes[2].to_i64() as u64;
+        let valued = codes[3].to_i64() != -1;
+        let value = codes[4].to_i64() as u64;
+
+        let demand = match codes[5].to_i64() {
+            -1 => Demand::Unassigned,
+            0 => Demand::Terrible,
+            1 => Demand::Low,
+            2 => Demand::Normal,
+            3 => Demand::High,
+            4 => Demand::Amazing,
+            _ => unreachable!(),
+        };
+
+        let trend = match codes[6].to_i64() {
+            -1 => Trend::Unassigned,
+            0 => Trend::Lowering,
+            1 => Trend::Unstable,
+            2 => Trend::Stable,
+            3 => Trend::Raising,
+            4 => Trend::Fluctuating,
+            _ => unreachable!(),
+        };
+
+        let projected = match codes[7].to_i64() {
+            1 => true,
+            -1 => false,
+            _ => unreachable!(),
+        };
+
+        let hyped = match codes[8].to_i64() {
+            1 => true,
+            -1 => false,
+            _ => unreachable!(),
+        };
+
+        let rare = match codes[9].to_i64() {
+            1 => true,
+            -1 => false,
+            _ => unreachable!(),
+        };
+
+        Ok(ItemDetails {
+            item_id,
+            item_name,
+            acronym,
+            rap,
+            valued,
+            value,
+            demand,
+            trend,
+            projected,
+            hyped,
+            rare,
+        })
+    }
+}
+
+impl AllItemDetailsResponse {
+    fn into_hashmap(self) -> Result<HashMap<u64, ItemDetails>, ItemsError> {
+        let mut item_details_map = HashMap::new();
+
+        for (item_id_string, codes) in self.items {
+            let item_id = match item_id_string.parse() {
+                Ok(x) => x,
+                Err(_) => return Err(ItemsError::MalformedResponse),
+            };
+
+            let item_details = ItemDetails::from_raw(item_id, codes)?;
+
+            item_details_map.insert(item_id, item_details);
+        }
+
+        Ok(item_details_map)
+    }
+}
+
 impl Client {
     /// A wrapper for <https://www.rolimons.com/itemapi/itemdetails>.
     ///
@@ -104,12 +213,12 @@ impl Client {
                             Err(_) => return Err(ItemsError::MalformedResponse),
                         };
 
-                        let item_details = AllItemDetails::from_raw_response(raw);
+                        let item_details = raw.into_hashmap()?;
 
                         Ok(item_details)
                     }
-                    429 => Err(ApiError::TooManyRequests(url)),
-                    _ => Err(ApiError::UnknownStatusCode(url, status_code)),
+                    429 => Err(ItemsError::TooManyRequests),
+                    _ => Err(ItemsError::UnidentifiedStatusCode(status_code)),
                 }
             }
             Err(e) => Err(ItemsError::ReqwestError(e)),
