b329660ac691775f1d47392f4682b06c74913ccf
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-02-22 23:53:03 -0500

    feat: clean up get_all_items example

--- examples/get_all_items.rs
+++ examples/get_all_items.rs
@@ -1,5 +1,6 @@
 #[tokio::main]
 async fn main() {
     let roli_client = roli::ClientBuilder::new().build();
-    dbg!(roli_client.all_item_details().await);
+    let all_item_details = roli_client.all_item_details().await.unwrap();
+    println!("Item Amount: {}", all_item_details.len());
 }
